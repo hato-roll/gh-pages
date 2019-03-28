34e1da075579a958db127ee54c620eae3c511e6b
hatolab <none>
2019-03-28 18:55:36 +0900

    それはそうじゃない時に確認する

--- sorehasou.js
+++ sorehasou.js
@@ -1,6 +1,18 @@
-function sorehasou(){
+function alert_sorehasou(){
     alert("それはそう");
 }
-function sorehasouzyanaino(){
+function alert_sorehasouzyanaino(){
     alert("それはそうじゃないの？");
+}
+
+// 将来的にはOKとキャンセルじゃなくてそれはそうかを聞きたい
+function confirm_sorehasouzyanaino(){
+    var let;
+    let=confirm("それはそうじゃないの？");
+    if (let){
+        location.href = 'https://www.google.com/';
+    }
+    else {
+        alert("それはそう");
+    }   
 }
